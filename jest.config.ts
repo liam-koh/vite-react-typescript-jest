188b6d63ed9152942b8b26adf541f61bea8e92b7
liam-koh <144200505+liam-koh@users.noreply.github.com>
2024-10-02 15:57:24 +0900

    commit

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,19 @@ export default {
   preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: "jsdom",
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': ['ts-jest', {
+      diagnostics: {
+        ignoreCodes: [1343]
+      },
+      astTransformers: {
+        before: [
+          {
+            path: 'ts-jest-mock-import-meta',  // or, alternatively, 'ts-jest-mock-import-meta' directly, without node_modules.
+            options: { metaObjectReplacement: { url: 'https://www.url.com' } }
+          }
+        ]
+      }
+    }],
     '^.+\\.jsx?$': 'babel-jest', // 추가
      "node_modules/@o2pluss+\\.(j|t)sx?$": "ts-jest"
   },
